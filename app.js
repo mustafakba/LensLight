91a65f7fcedfe95be870459d44b8ae56c2b383e8
mustafakba <kaba.mustafaa@gmail.com>
2023-07-09 22:04:57 +0300

    ejs added to project . html files change to ejs

--- app.js
+++ app.js
@@ -3,8 +3,20 @@ import express from "express"
 const app = express()
 const port = 3000
 
+//ejs template engine
+app.set("view engine","ejs")
+
+//static files middleware
+app.use(express.static('public'));
+
+//home page
 app.get("/",(req,res)=>{
-    res.send('INDEX SAYFASI 2');
+    res.render("index");
+})
+
+//about page
+app.get("/about",(req,res)=>{
+    res.render("about");
 })
 app.listen(port, ()=> {
     console.log(`Application running on this port ${port}`)
