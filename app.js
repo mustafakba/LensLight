62aa00d43ed05436dfb16142436933467c9adad0
mustafakba <kaba.mustafaa@gmail.com>
2023-07-12 23:35:08 +0300

    page routes and page controllers created

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import conn from "./db.js";
+import pageRoute from "./routes/pageRoute.js";
 
 dotenv.config()
 
@@ -16,15 +17,18 @@ app.set("view engine","ejs")
 //static files middleware
 app.use(express.static('public'));
 
+//routes
+app.use("/",pageRoute)
+
 //home page
-app.get("/",(req,res)=>{
-    res.render("index");
-})
+//app.get("/",(req,res)=>{
+//    res.render("index");
+//})
 
 //about page
-app.get("/about",(req,res)=>{
-    res.render("about");
-})
+//app.get("/about",(req,res)=>{
+//    res.render("about");
+//})
 app.listen(port, ()=> {
     console.log(`Application running on this port ${port}`)
 })
