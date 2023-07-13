e5527d81ba45aa437f07c8a9324696475dabcf2c
mustafakba <kaba.mustafaa@gmail.com>
2023-07-13 23:12:25 +0300

    Photos are dynamically placed on the homepage

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,8 +1,12 @@
-const getIndexPage = (req,res) =>{
-    res.render("index")
-}
+const getIndexPage = (req, res) => {
+  res.render("index", {
+    link: "index",
+  });
+};
 
-const getAboutPage = (req,res) =>{
-    res.render("about.ejs")
-}
-export {getIndexPage,getAboutPage}
+const getAboutPage = (req, res) => {
+  res.render("about.ejs", {
+    link: "about",
+  });
+};
+export { getIndexPage, getAboutPage };
--- routes/photoRoute.js
+++ routes/photoRoute.js
@@ -3,10 +3,7 @@ import photoController from "../controllers/photoController.js";
 
 const router = express.Router();
 
-router
-  .route("/")
-  .get(photoController.getAllPhotos)
-  .router.route("/")
-  .post(photoController.createPhoto);
+router.route("/").get(photoController.getAllPhotos);
+router.route("/").post(photoController.createPhoto);
 
 export default router;
