5e00e0da64da05224517c3041dec3f4afa0d4106
mustafakba <kaba.mustafaa@gmail.com>
2023-07-24 23:53:27 +0300

    user validation created from backend

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,12 +4,23 @@ import jwt from "jsonwebtoken";
 const createUser = async (req, res) => {
   try {
     const user = await User.create(req.body);
-    res.status(201).redirect("/login");
+    res.status(201).json({ user: user._id });
   } catch (error) {
-    res.status(500).json({
-      success: false,
-      error,
-    });
+    let errors2 = {};
+
+    if (error.code === 11000) {
+      errors2.email = "The email is already registered";
+    }
+
+    if (error.name === "ValidationError") {
+      Object.keys(error.errors).forEach((key) => {
+        errors2[key] = error.errors[key].message;
+      });
+    }
+
+    console.log("ERRORS 2 ::::", errors2);
+
+    res.status(400).json(errors2);
   }
 };
 
