db8229e947672cc21c79d8b793363b0ddd96ec83
mustafakba <kaba.mustafaa@gmail.com>
2023-07-29 19:40:51 +0300

    follow & unf buttons func and front created . But status bug error still continue

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,25 +1,26 @@
-import User from "../models/userModel.js";
-import Photo from "../models/photoModel.js";
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
+import User from '../models/userModel.js';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import Photo from '../models/photoModel.js';
+
 const createUser = async (req, res) => {
   try {
     const user = await User.create(req.body);
     res.status(201).json({ user: user._id });
   } catch (error) {
+    console.log('ERROR', error);
+
     let errors2 = {};
 
     if (error.code === 11000) {
-      errors2.email = "The email is already registered";
+      errors2.email = 'The Email is already registered';
     }
 
-    if (error.name === "ValidationError") {
+    if (error.name === 'ValidationError') {
       Object.keys(error.errors).forEach((key) => {
         errors2[key] = error.errors[key].message;
       });
     }
-
-    console.log("ERRORS 2 ::::", errors2);
 
     res.status(400).json(errors2);
   }
@@ -38,21 +39,22 @@ const loginUser = async (req, res) => {
     } else {
       return res.status(401).json({
         success: false,
-        error: "There is no such user",
+        error: 'There is no such user',
       });
     }
 
     if (same) {
       const token = createToken(user._id);
-      res.cookie("jwt", token, {
+      res.cookie('jwt', token, {
         httpOnly: true,
         maxAge: 1000 * 60 * 60 * 24,
       });
-      res.redirect("/users/dashboard");
+
+      res.redirect('/users/dashboard');
     } else {
       res.status(401).json({
         success: false,
-        error: "Paswords are not matched",
+        error: 'Paswords are not matched',
       });
     }
   } catch (error) {
@@ -65,27 +67,26 @@ const loginUser = async (req, res) => {
 
 const createToken = (userId) => {
   return jwt.sign({ userId }, process.env.JWT_SECRET, {
-    expiresIn: "1d",
+    expiresIn: '1d',
   });
 };
 
 const getDashboardPage = async (req, res) => {
   const photos = await Photo.find({ user: res.locals.user._id });
-  const user = await User.findById(res.locals.user._id ).populate(["followings","followers"])
+  const user = await User.findById(res.locals.user._id ).populate(["followings","followers"]);
 
-  res.render("dashboard.ejs", {
-    link: "dashboard",
+  res.render('dashboard', {
+    link: 'dashboard',
     photos,
   });
 };
 
-
 const getAllUsers = async (req, res) => {
   try {
-    const users = await User.find({_id:{$ne:res.locals.user._id}});
-    res.status(200).render("users", {
+    const users = await User.find({ _id: { $ne: res.locals.user._id } });
+    res.status(200).render('users', {
       users,
-      link: "users",
+      link: 'users',
     });
   } catch (error) {
     res.status(500).json({
@@ -94,16 +95,21 @@ const getAllUsers = async (req, res) => {
     });
   }
 };
-
 
 const getAUser = async (req, res) => {
   try {
-    const user = await User.findById({ _id: req.params.id })
-    const photos = await Photo.find({user:user._id })
-    res.status(200).render("user", {
+    const user = await User.findById({ _id: req.params.id });
+
+    const inFollowers = user.followers.some(()=>{
+      return follower.equals(res.locals.user._id)
+    })
+
+    const photos = await Photo.find({ user: user._id });
+    res.status(200).render('user', {
       user,
       photos,
-      link: "users",
+      link: 'users',
+      inFollowers
     });
   } catch (error) {
     res.status(500).json({
@@ -112,24 +118,28 @@ const getAUser = async (req, res) => {
     });
   }
 };
+
 const follow = async (req, res) => {
+  // res.locals.user._id
   try {
     let user = await User.findByIdAndUpdate(
-        {_id:req.params.id},
-        {$push:{followers:res.locals.res._id}},
-        {new:true}
-    )
+        { _id: req.params.id },
+        {
+          $push: { followers: res.locals.user._id },
+        },
+        { new: true }
+    );
 
     user = await User.findByIdAndUpdate(
-        {_id:res.locals.user._id},
-        {$push:{following:res.params.id}},
-         {new:true}
-    )
+        { _id: res.locals.user._id },
+        {
+          $push: { followings: req.params.id },
+        },
+        { new: true }
+    );
 
-    res.status(200).json({
-      success:true,
-      user
-    })
+    res.status(200).redirect(`/users/${req.params.id}`);
+
   } catch (error) {
     res.status(500).json({
       success: false,
@@ -138,29 +148,39 @@ const follow = async (req, res) => {
   }
 };
 
-const unfollow = async (req,res) => {
+const unfollow = async (req, res) => {
+  // res.locals.user._id
   try {
     let user = await User.findByIdAndUpdate(
-        {_id:req.params.id},
-        {$pull:{followers:res.locals.user._id}},
-        {new:true}
-    )
+        { _id: req.params.id },
+        {
+          $pull: { followers: res.locals.user._id },
+        },
+        { new: true }
+    );
 
     user = await User.findByIdAndUpdate(
-        {_id:req.locals.user._id},
-        {$pull:{following:req.params.id}},
-        {new:true}
-    )
-    res.status(200).json({
-      success:true,
-      user,
-    })
-  }
-  catch (error) {
+        { _id: res.locals.user._id },
+        {
+          $pull: { followings: req.params.id },
+        },
+        { new: true }
+    );
+    res.status(200).redirect(`/users/${req.params.id}`)
+  } catch (error) {
     res.status(500).json({
       success: false,
       error,
     });
   }
 };
-export { createUser, loginUser, getDashboardPage,getAllUsers,getAUser,follow,unfollow };
+
+export {
+  createUser,
+  loginUser,
+  getDashboardPage,
+  getAllUsers,
+  getAUser,
+  follow,
+  unfollow,
+};
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -9,7 +9,7 @@ router.route('/login').post(userController.loginUser);
 router.route('/dashboard').get(authMiddleware.authenticateToken,userController.getDashboardPage);
 router.route('/').get(authMiddleware.authenticateToken,userController.getAllUsers)
 router.route('/:id').get(authMiddleware.authenticateToken,userController.getAUser)
-router.route('/:id').put(authMiddleware.authenticateToken,userController.follow)
-router.route('/:id').put(authMiddleware.authenticateToken,userController.unfollow)
+router.route('/:id/follow').put(authMiddleware.authenticateToken,userController.follow)
+router.route('/:id/unfollow').put(authMiddleware.authenticateToken,userController.unfollow)
 
 export default router;
